c3808912b48f3ac4c229673d91dcd9e1901cc803
Isaque_Alvess <ysak.alves@outlook.com>
2024-01-27 01:23:07 -0300

    adding some html pages and creating candidateController

---
+++ src/main/java/com/isaquealves/front_gestao_vagas/modules/candidate/service/CandidateService.java
@@ -0,0 +1,36 @@
+package com.isaquealves.front_gestao_vagas.modules.candidate.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.isaquealves.front_gestao_vagas.modules.candidate.dto.Token;
+
+@Service
+public class CandidateService {
+    
+
+    public Token login(String username, String password) {
+        RestTemplate rt = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, String> data = new HashMap<>();
+        data.put("username", username);
+        data.put("password", password);
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(data, headers);
+
+        var result = rt.postForObject("http://localhost:8080/candidate/auth", request, Token.class);
+        System.out.println("Retorno da requisição");
+        System.out.println(result.getAccess_token());
+
+        return result;
+    }
+}
